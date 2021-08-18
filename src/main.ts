d16edb7bd5b57c6d4648e87c60eb9365c823e784
Resilient923 <kc960923@naver.com>
2021-08-19 00:00:28 +0900

    [update] swaggerSetting 210819

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,66 @@
 import {NestFactory} from '@nestjs/core';
-import {AppModule} from './app.module';
+import {NestExpressApplication} from '@nestjs/platform-express';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
+import passport from 'passport';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import path from 'path';
+import {ValidationPipe} from '@nestjs/common';
 
+import {AppModule} from './app.module';
+import {HttpExceptionFilter} from './http-exception.filter';
 declare const module: any;
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.useGlobalFilters(new HttpExceptionFilter());
+    app.useGlobalPipes(
+        new ValidationPipe({
+            transform: true,
+        }),
+    );
+    app.enableCors({
+        origin: true,
+        credentials: true,
+    });
+    // app.useStaticAssets(
+    //     process.env.NODE_ENV === 'production'
+    //         ? path.join(__dirname, '..', '..', 'uploads')
+    //         : path.join(__dirname, '..', 'uploads'),
+    //     {
+    //         prefix: '/uploads',
+    //     },
+    // );
+    // app.useStaticAssets(
+    //     process.env.NODE_ENV === 'production'
+    //         ? path.join(__dirname, '..', '..', 'public')
+    //         : path.join(__dirname, '..', 'public'),
+    //     {
+    //         prefix: '/dist',
+    //     },
+    // );
+    const config = new DocumentBuilder()
+        .setTitle('Sleact API')
+        .setDescription('Sleact 개발을 위한 API 문서입니다.')
+        .setVersion('1.0')
+        .addCookieAuth('connect.sid')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('aapi', app, document);
+
+    app.use(cookieParser());
+    app.use(
+        session({
+            resave: false,
+            saveUninitialized: false,
+            secret: process.env.COOKIE_SECRET,
+            cookie: {
+                httpOnly: true,
+            },
+        }),
+    );
+    app.use(passport.initialize());
+    app.use(passport.session());
     const port = process.env.PORT || 3000;
     await app.listen(port);
     console.log(`listening on port ${port}`);

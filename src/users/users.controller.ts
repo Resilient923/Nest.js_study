2e158268542fd7a7e7493b124ebb4ed0f10c6c4f
Resilient923 <kc960923@naver.com>
2021-08-21 22:25:02 +0900

    [update] controllerSwaggerSetting 210821

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,25 +1,30 @@
 import {Body, Controller, Get, Post, Req, Res} from '@nestjs/common';
+import {ApiOperation} from '@nestjs/swagger';
 import {JoinRequestDto} from './dto/join.request.dto';
 import {UsersService} from './users.service';
 
 @Controller('api/users')
 export class UsersController {
     constructor(private userService: UsersService) {}
+    @ApiOperation({summary: '내 정보 조회'})
     @Get()
     getUsers(@Req() req) {
         return req.user;
     }
 
+    @ApiOperation({summary: '회원가입'})
     @Post()
     postUsers(@Body() data: JoinRequestDto) {
         this.userService.postUsers(data.email, data.nickname, data.password);
     }
 
+    @ApiOperation({summary: '로그인'})
     @Post('login')
     logIn(@Req() req) {
         return req.user;
     }
 
+    @ApiOperation({summary: '로그아웃'})
     @Post('logout')
     logOut(@Req() req, @Res() res) {
         req.logOut();

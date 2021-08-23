a27e271094c8d3864a9013565097508e32c3d883
Resilient923 <kc960923@naver.com>
2021-08-23 18:24:57 +0900

    [update] loggerSetting 210823

--- src/middlewares/logger.middleware.ts
+++ src/middlewares/logger.middleware.ts
@@ -1,24 +1,24 @@
-import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
-import { NextFunction, Response, Request } from 'express';
+import {Injectable, Logger, NestMiddleware} from '@nestjs/common';
+import {NextFunction, Response, Request} from 'express';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-  //디버그라는 패키지역할
-  private logger = new Logger('HTTP');
+    //디버그라는 패키지역할
+    private logger = new Logger('HTTP');
 
-  use(request: Request, response: Response, next: NextFunction): void {
-    const { ip, method, originalUrl } = request;
-    const userAgent = request.get('user-agent') || '';
+    use(request: Request, response: Response, next: NextFunction): void {
+        const {ip, method, originalUrl} = request;
+        const userAgent = request.get('user-agent') || '';
 
-    response.on('finish', () => {
-      const { statusCode } = response;
-      const contentLength = response.get('content-length');
-      this.logger.log(
-        //Logger.log
-        `${method} ${originalUrl} ${statusCode} ${contentLength} - ${userAgent} ${ip}`,
-      );
-    });
+        response.on('finish', () => {
+            const {statusCode} = response;
+            const contentLength = response.get('content-length');
+            this.logger.log(
+                //Logger.log
+                `${method} ${originalUrl} ${statusCode} ${contentLength} - ${userAgent} ${ip}`,
+            );
+        });
 
-    next();
-  }
+        next();
+    }
 }

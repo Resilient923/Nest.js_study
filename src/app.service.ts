baf333690185ab221a31b4e1b548208ceb69e856
Resilient923 <kc960923@naver.com>
2021-09-09 23:03:39 +0900

    [add] controller 210909

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,15 @@
 import {Injectable} from '@nestjs/common';
+import {UsersService} from './users/users.service';
 
 //요청, 응답에 대해서는 모른다.
 @Injectable()
 export class AppService {
+    constructor(private userService: UsersService) {}
     async getHello() {
+        // this.userService.getUser();
+        // this.getWow();
         return process.env.SECRET;
     }
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    async getWow() {}
 }

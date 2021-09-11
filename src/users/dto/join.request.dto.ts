e574739052f136f0b4ac31c18b26b5d8eeedf20e
Resilient923 <kc960923@naver.com>
2021-09-11 23:09:20 +0900

    [ad]] userDto 210911

---
+++ src/users/dto/join.request.dto.ts
@@ -0,0 +1,28 @@
+import {ApiProperty} from '@nestjs/swagger';
+import {IsEmail, IsNotEmpty, IsString} from 'class-validator';
+
+export class JoinRequestDto {
+    @IsEmail()
+    @ApiProperty({
+        example: 'skc@gmail.com',
+        description: '이메일',
+        required: true,
+    })
+    public email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @ApiProperty({
+        example: 'skc',
+        description: '닉네임',
+    })
+    public nickname: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @ApiProperty({
+        example: 'nodejsbook',
+        description: '비밀번호',
+    })
+    public password: string;
+}
